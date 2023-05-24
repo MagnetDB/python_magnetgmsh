b8069741bb7073c1652b56c4716d8ceabc95bdc2
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2023-05-24 11:29:50 +0200

    use same naming for salome/gmsh

--- python_magnetgmsh/Bitters.py
+++ python_magnetgmsh/Bitters.py
@@ -102,7 +102,12 @@ def gmsh_ids(
 
 
 def gmsh_bcs(
-    Bitters, mname: str, ids: tuple, thickslit: bool = False, skipR: bool = False, debug: bool = False
+    Bitters,
+    mname: str,
+    ids: tuple,
+    thickslit: bool = False,
+    skipR: bool = False,
+    debug: bool = False,
 ) -> dict:
     """
     retreive ids for bcs in gmsh geometry
@@ -112,6 +117,10 @@ def gmsh_bcs(
     print(f"gmsh_bcs: Bitters={Bitters.name}, mname={mname}, thickslit={thickslit}")
     (gmsh_ids, crack_ids, Air_data) = ids
     # print("Bitters/gmsh_bcs:", ids)
+
+    prefix = ""
+    if mname:
+        prefix = f"{mname}_"
 
     defs = {}
     bcs_defs = {}
@@ -127,7 +136,7 @@ def gmsh_bcs(
         print(f"Bitters/gmsh/{Bitters.magnets} (str)")
         with open(f"{Bitters.magnets}.yaml", "r") as f:
             Object = yaml.load(f, Loader=yaml.FullLoader)
-        defs.update(load_defs(Object, f"{Bitters.name}_{Object.name}", ids))
+        defs.update(load_defs(Object, f"{prefix}{Object.name}", ids))
 
     elif isinstance(Bitters.magnets, list):
         print(f"Bitters/gmsh/{Bitters.magnets} (list)")
@@ -137,46 +146,48 @@ def gmsh_bcs(
             with open(f"{mname}.yaml", "r") as f:
                 Object = yaml.load(f, Loader=yaml.FullLoader)
             _ids = (gmsh_ids[num], crack_ids[num], ())
-            defs.update(load_defs(Object, f"{Bitters.name}_{Object.name}", _ids))
+            defs.update(load_defs(Object, f"{prefix}{Object.name}", _ids))
             Nslits = len(Object.coolingslits)
             if i == 0:
                 print(f"Bitters/gmsh/{mname} Bitter[{i}] rInt: {defs}")
-                key = f"{Bitters.name}_{Object.name}_Slit0"
+                key = f"{prefix}{Object.name}_Slit0"
                 bcs_def = [Object.r[0], Object.z[0], Object.r[0], Object.z[1]]
                 defs[key] = create_bcs(key, bcs_def, 1)
                 # print(f'{key}: bcs_def={defs[key]}')
-                    
+
                 # print(f'Bitters rInt: defs={defs}')
 
             # add Bitter.magnets[i+1]_rInt + Bitter.magnets[i]_rExt -> Bitter.magnets[i]_slitN
-            if i < len(Bitters.magnets) -1:
-                nmname = Bitters.magnets[i+1]
-                print(f"Bitters/gmsh/{mname} Bitter[{i}] rExt {nmname} Bitter[{i+1}] rInt: {defs}")
+            if i < len(Bitters.magnets) - 1:
+                nmname = Bitters.magnets[i + 1]
+                print(
+                    f"Bitters/gmsh/{mname} Bitter[{i}] rExt {nmname} Bitter[{i+1}] rInt: {defs}"
+                )
                 with open(f"{nmname}.yaml", "r") as f:
                     nObject = yaml.load(f, Loader=yaml.FullLoader)
-                    _nids = (gmsh_ids[num+1], crack_ids[num+1], ())
+                    _nids = (gmsh_ids[num + 1], crack_ids[num + 1], ())
 
-                    key = f"{Bitters.name}_{Object.name}_Slit{Nslits+1}"
+                    key = f"{prefix}{Object.name}_Slit{Nslits+1}"
                     bcs_def = [
                         [Object.r[1], Object.z[0], Object.r[1], Object.z[1]],
-                        [nObject.r[0], nObject.z[0], nObject.r[0], nObject.z[1]]
+                        [nObject.r[0], nObject.z[0], nObject.r[0], nObject.z[1]],
                     ]
                     defs[key] = create_bcs(key, bcs_def, 1)
                     # print(f'{key}: bcs_def={defs[key]}')
-                    
+
                 # print(f'Bitters rInt/rExt: defs={defs}')
 
-            if i == len(Bitters.magnets) -1 :
+            if i == len(Bitters.magnets) - 1:
                 print(f"Bitters/gmsh/{mname} Bitter[{i}] rExt: {defs}")
-                key = f"{Bitters.name}_{Object.name}_Slit{Nslits+1}"
+                key = f"{prefix}{Object.name}_Slit{Nslits+1}"
                 bcs_def = [Object.r[1], Object.z[0], Object.r[1], Object.z[1]]
                 defs[key] = create_bcs(key, bcs_def, 1)
                 # print(f'{key}: bcs_def={defs[key]}')
                 # print(f'Bitters rExt: defs={defs}')
-                
+
             num += 1
 
-    print(f'Bitters: defs={defs.keys()}')
+    print(f"Bitters: defs={defs.keys()}")
     # rename Bitter.magnets[0]_rInt -> Bitter.magnets[0]_slit0
     # add Bitter.magnets[i]_rExt + Bitter.magnets[i+1]_rInt -> Bitter.magnets[i+1]_slit0
     # remove Bitter.magnets[i]_rExt and Bitter.magnets[i+1]_rInt
