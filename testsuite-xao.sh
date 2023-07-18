e70d9dd87a7af53173a299c7a30127963c056d6c
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2023-07-18 12:51:43 +0000

    up

--- testsuite-xao.sh
+++ testsuite-xao.sh
@@ -5,7 +5,7 @@
 #TODO: make optionnal set -x
 
 TestWD="/data/geometries"
-TestsAxi="test M9_Be M9Bitters M9_HLtest Oxford1 Oxford HTS-dblpancake-test2 HTS-pancake-test2 HTS-tape-test2 MNougat"
+TestsAxi="test M9_Be M9Bitters M9_HLtest Oxford1 Oxford HTS-dblpancake-test2 HTS-pancake-test2 HTS-tape-test2 Nougat MNougat"
 
 echo_success() {
   echo -en "[\033[32m  OK  \033[39m]"
@@ -29,10 +29,11 @@ echo "Axi Mesh generation with Gmsh"
 for test in ${TestsAxi}; do
     echo -en "${test} : "
     if [ -f ${TestWD}/${test}-Axi.xao ]; then  
-        python -m python_magnetgmsh.xao2msh ${test}-Axi.xao --geo ${test}.yaml --wd ${TestWD} mesh --group CoolingChannels > ${test}_mesh.log 2>&1
+        python -m python_magnetgmsh.xao2msh ${test}-Axi.xao --geo ${test}.yaml --wd ${TestWD} mesh --group CoolingChannels > ${test}-xao_mesh.log 2>&1
         status=$?
         if [ "$status" != "0" ]; then
 	        echo_failure
+          exit 1
         else
 	        echo_success
         fi
@@ -45,10 +46,11 @@ echo "Axi Mesh generation with Gmsh (with Air)"
 for test in ${TestsAxi}; do
     echo -en "${test} : " 
     if [ -f ${TestWD}/${test}-Axi_withAir.xao ]; then  
-        python -m python_magnetgmsh.xao2msh ${test}-Axi_withAir.xao --geo ${test}.yaml --wd ${TestWD} mesh --group CoolingChannels > ${test}_withAir_mesh.log 2>&1
+        python -m python_magnetgmsh.xao2msh ${test}-Axi_withAir.xao --geo ${test}.yaml --wd ${TestWD} mesh --group CoolingChannels > ${test}-xao_withAir_mesh.log 2>&1
         status=$?
         if [ "$status" != "0" ]; then
 	        echo_failure
+          exit 1
         else
 	        echo_success
         fi
