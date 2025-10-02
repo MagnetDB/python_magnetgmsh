c352bf6b29f251110d874d0484a6bd4260da97da
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2025-10-02 11:36:14 +0000

    fix python version

--- setup.py
+++ setup.py
@@ -17,17 +17,12 @@ test_requirements = ["pytest>=3"]
 setup(
     author="Christophe Trophime",
     author_email="christophe.trophime@lncmi.cnrs.fr",
-    python_requires=">=3.5",
+    python_requires=">=3.9"
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     description="Python Magnet Geometry contains maget geometrical models",
     entry_points={
