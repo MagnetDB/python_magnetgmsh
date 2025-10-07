8305a62556144a3ef28ac8d64a90e20a2885aa9c
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2025-10-07 09:54:47 +0200

    add testsuite for 2D

---
+++ testsuite2D.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+
+# set -euo pipefail
+#TODO: make optionnal set -x
+
+TestWD="/data/geometries"
+TestsAxi="M9_Be"
+#TestsAxi="M9_Be M9Bitters M9_HLtest"
+
+echo_success() {
+  echo -en "[\033[32m  OK  \033[39m]"
+  echo 
+  return 0
+}
+
+echo_failure() {
+  echo -en "[\033[31mFAILED\033[39m]"
+  echo 
+  return 1
+}
+
+echo "2D CAD generation"
+for test in ${TestsAxi}; do
+    echo -en "${test} : " 
+    python -m python_magnetgmsh.Bitter2D ${test}.yaml --wd ${TestWD} --thickslit > ${test}_gmsh.log 2>&1
+    status=$?
+    if [ "$status" != "0" ]; then
+	    echo_failure
+      exit 1
+    else
+	    echo_success
+    fi
+done
+
+echo "2D Mesh generation with Gmsh"
+for test in ${TestsAxi}; do
+    echo -en "${test} : " 
+    python -m python_magnetgmsh.Bitter2D ${test}.yaml --wd ${TestWD} --thickslit --mesh > ${test}_mesh_gmsh.log 2>&1
+    status=$?
+    if [ "$status" != "0" ]; then
+	    echo_failure
+      exit 1
+    else
+	    echo_success
+    fi
+done
+
+echo "2D CAD generation"
+for test in ${TestsAxi}; do
+    echo -en "${test} : " 
+    python -m python_magnetgmsh.Bitter2Dquarter ${test}.yaml --wd ${TestWD} --thickslit > ${test}_gmsh.log 2>&1
+    status=$?
+    if [ "$status" != "0" ]; then
+	    echo_failure
+      exit 1
+    else
+	    echo_success
+    fi
+done
+
+echo "2D Mesh generation with Gmsh"
+for test in ${TestsAxi}; do
+    echo -en "${test} : " 
+    python -m python_magnetgmsh.Bitter2Dquarter ${test}.yaml --wd ${TestWD} --thickslit --mesh > ${test}_mesh_gmsh.log 2>&1
+    status=$?
+    if [ "$status" != "0" ]; then
+	    echo_failure
+      exit 1
+    else
+	    echo_success
+    fi
+done
+
